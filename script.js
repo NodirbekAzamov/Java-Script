d34572ac6966390ccc52b5e5f26153b46f5980e8
nodirbek3 <90841160+nodirbek3@users.noreply.github.com>
2021-09-16 16:47:44 +0500

    fonkshnlar

---
+++ script.js
@@ -0,0 +1,73 @@
+// var krm=100
+// var chqm=90
+// if ( krm>chqm) {console.log("foyda qildik");
+// }else if (krm>chqm) {console.log("bir xil");
+// }else{
+//     console.log("zara kordik");
+// }
+// var bal=100
+// var bal1=90
+// var bal2=80
+// var bal3=70
+// var bal4=60
+// var bal5=50
+// var bal6=40
+// var bal=0
+// if (bal<bal1) {console.log("5");
+// }else if (bal1<bal2) {console.log("4");
+// }else if (bal2<bal3) {console.log("3");
+// }else if (bal3<bal4) {console.log("2");
+// }else if (bal4<bal5) {console.log("1");
+// }else if (bal5<bal6) {console.log(0); 
+// }else{console.log(0);}
+
+//  length nechta element borligi sanash
+// for (let index = 100; index >= 0; index--) {
+//     console.log(index);
+    
+// }
+// for(let i=0; i<100; i++){
+//     if ((i%7) ==0) {
+//         console.log(i);
+        
+//     }
+// } 
+//  for (let index = 0; index < array.length; index++) {
+//     }
+
+// var pl=["Python", "Javascript","PHP","rust"]
+// for (var i =0;  i<pl.length; i++;)  {
+//     const i = arvar i =0;
+     
+// }
+// console.log("*" .repeat(1));
+// console.log("*" .repeat(2));
+// console.log("*" .repeat(3));
+// console.log("*" .repeat(4));
+// console.log("*" .repeat(5));
+// console.log("*" .repeat(6));
+// console.log("*" .repeat(7));
+// console.log("*" .repeat(8));
+// console.log("*" .repeat(9));
+// console.log("*" .repeat(10));
+// for(var i=0; i<10; i++){
+//      console.log("*".repeat(i));
+//  console.log(" " .repeat(6)+ "*" .repeat(1)+ " " .repeat(6));
+//  console.log(" " .repeat(5)+ "*" .repeat(3)+ " " .repeat(5));
+//  console.log(" " .repeat(4)+ "*" .repeat(5)+ " " .repeat(4));
+//  console.log(" " .repeat(3)+ "*" .repeat(7)+ " " .repeat(3));
+// console.log(" " .repeat(2)+ "*" .repeat(9)+ " " .repeat(2));
+// var j=0
+// for(var i=8; i>0;i--)
+// {
+//     console.log(" "  .repeat(i)+"/"+  "*" .repeat(j)+ "*"+"*" .repeat(j) );
+//     j++
+// }
+
+
+
+var j = 0
+for (let i = 10; i > 0; i--) {
+  console.log("".repeat(i) + "*".repeat(j) + "*" + "*".repeat)(j);
+    
+}
